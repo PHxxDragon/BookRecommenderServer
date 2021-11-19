ff2c5c66219facde1025ca40323531d8b7712246
Nguyen Do Quoc Duy <duy.nguyenbkcs@hcmut.edu.vn>
2021-11-19 13:56:26 +0700

    initial commit

---
+++ src/main/java/com/is/bookrecommender/BookRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.is.bookrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRecommenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/is/bookrecommender/BookRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.bookrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
